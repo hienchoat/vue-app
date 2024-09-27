add0e89f37e512ad903e623aa916d97d8a40ccb4
hienchoat <hienchoat05@gmail.com>
2024-09-27 12:21:02 +0700

    hien comit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderComponent from "./components/HeaderComponent.vue"
+import HomeComponent from "./components/HomeComponent.vue"
+
+</script>
+
+<template>
+  test
+  <HeaderComponent/>
+  <HomeComponent/>
+
+</template>
+
+<style scoped>
+
+</style>
